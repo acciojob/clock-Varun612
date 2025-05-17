e1d5b066b20d6bc4a5ee31877483392be62c511c
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-17 08:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+timerEl=document.getElementById("timer");
+let currentDate=new Date();
+setInterval( function(){
+	currentDate=new Date();
+	timerEl.innerHTML=currentDate.toLocaleString();
+},1000)
